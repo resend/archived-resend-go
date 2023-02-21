48499e2f0b8dcfb1bde9b3774e0b513fd9c453a4
simplesagar <sagar.batchu92@gmail.com>
2023-02-20 20:25:34 -0800

    chore: remove retry config

--- pkg/models/operations/sendemail.go
+++ pkg/models/operations/sendemail.go
@@ -2,12 +2,10 @@ package operations
 
 import (
 	"github.com/resendlabs/resend-go/pkg/models/shared"
-	"github.com/resendlabs/resend-go/pkg/models/utils"
 )
 
 type SendEmailRequest struct {
 	Request shared.Email `request:"mediaType=application/json"`
-	Retries *utils.RetryConfig
 }
 
 type SendEmailResponse struct {
--- pkg/models/utils/sendemail.go
+++
@@ -1,4 +0,0 @@
-package utils
-
-type RetryConfig struct {
-}
